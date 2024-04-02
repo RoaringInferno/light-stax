3d7bf7e9f684f3a237049a1f033992f6ef92d51d
RoaringInferno <jj2t17@gmail.com>
2024-04-01 19:19:26 -0500

    Documented stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,45 +1,105 @@
+
 #pragma once
 
 #include <cstddef>
 
-#define STACK_TEMPLATE template<typename Size_T, typename T>
+#define STACK_TEMPLATE template <typename Size_T, typename T>
 #define STACK stack<Size_T, T>
 
-#define LINK_TEMPLATE template<typename T>
+#define LINK_TEMPLATE template <typename T>
 #define LINK link<T>
 
 namespace lstax
 {
-LINK_TEMPLATE
-class link {
-public:
-T* data; // Pointer to stored data.
-LINK* next; // Pointer to the next link.
-};
+    LINK_TEMPLATE
+    /**
+     * @brief A link for a linked list implementation of a stack.
+     * 
+     * @tparam T The type of data stored in the stack.
+     */
+    class link
+    {
+    public:
+        T *data;    // Pointer to stored data.
+        LINK *next; // Pointer to the next link.
+    };
 
-STACK_TEMPLATE
-class stack {
-public:
-stack(); // Empty constructor
+    STACK_TEMPLATE
+    /**
+     * @brief An abstract base class for a stack.
+     * 
+     * @tparam Size_T The type used for size/count values.
+     * @tparam T The type of data stored in the stack.
+     */
+    class stack
+    {
+    public:
+        /**
+         * @brief Constructs an empty stack.
+         */
+        stack(); // Empty constructor
 
-virtual Size_T length() = 0; // Returns the length
+        /**
+         * @brief Returns the length of the stack.
+         * 
+         * @return The length of the stack.
+         */
+        virtual Size_T length() = 0;
 
-virtual void push(const T* _data) = 0; // Adds a link with the given data.
-virtual void push(const T& _data) = 0; // Adds a link with the given data.
+        /**
+         * @brief Adds a link with the given data to the top of the stack.
+         * 
+         * @param _data The data to be added to the stack.
+         */
+        virtual void push(const T *_data) = 0;
 
-virtual T* top() = 0; // Views the top element.
-virtual void pop() = 0;  // Removes the top element.
-virtual void pop(T* _data) = 0;  // Removes the top element. Sets the given pointer to the data.
-virtual T* popOff() = 0; // Returns the held data. Requires 1 additional pointer.
+        /**
+         * @brief Adds a link with the given data to the top of the stack.
+         * 
+         * @param _data The data to be added to the stack.
+         */
+        virtual void push(const T &_data) = 0;
 
-virtual T* operator[](Size_T _index) = 0; // Indexing operator
-};
+        /**
+         * @brief Views the top element of the stack.
+         * 
+         * @return A pointer to the top element of the stack.
+         */
+        virtual T *top() = 0;
+
+        /**
+         * @brief Removes the top element from the stack.
+         */
+        virtual void pop() = 0;
+
+        /**
+         * @brief Removes the top element from the stack and sets the given pointer to the data.
+         * 
+         * @param _data A pointer to store the data of the removed element.
+         */
+        virtual void pop(T *_data) = 0;
+
+        /**
+         * @brief Removes the top element from the stack and returns the held data.
+         * 
+         * @return A pointer to the held data. Requires 1 additional pointer.
+         */
+        virtual T *popOff() = 0;
+
+        /**
+         * @brief Accesses the element at the specified index in the stack.
+         * 
+         * @param _index The index of the element to access.
+         * @return A pointer to the element at the specified index.
+         */
+        virtual T *operator[](Size_T _index) = 0;
+    };
 } // namespace lstax
 
 namespace lstax
 {
-STACK_TEMPLATE
-STACK::stack() {};
+    STACK_TEMPLATE
+    STACK::stack(){};
 } // namespace lstax
 
 #undef STACK_TEMPLATE
