fb0e892769569691ccd9898adc0905ff45684de0
RoaringInferno <jj2t17@gmail.com>
2024-04-06 17:57:00 -0500

    Doc for fifo_list

--- src/linked_list/fifo_list.hpp
+++ src/linked_list/fifo_list.hpp
@@ -9,14 +9,38 @@
 
 namespace lstax
 {
+    /**
+     * @brief Template parameter for the FIFO list.
+     * 
+     * This template parameter is used to define the type of data stored in the FIFO list.
+     * It allows the user to specify the data type when creating an instance of the FIFO list.
+     * 
+     * @tparam T The type of data stored in the FIFO list.
+     */
     FIFO_LIST_TEMPLATE
+    /**
+     * @brief A First-In-First-Out (FIFO) linked list implementation.
+     *
+     * This class represents a FIFO list, where elements are added to the end of the list and removed from the front.
+     * It is implemented using a linked list data structure.
+     */
     struct fifo_list : LINKED_LIST
     {
+        /**
+         * @brief Represents a node in a linked list.
+         */
         LINK* bottom;
 
+        
         fifo_list() : LINKED_LIST() {};
+        /**
+         * @brief Destructor for the fifo_list class.
+         * 
+         * This destructor is responsible for cleaning up any resources used by the fifo_list class.
+         * It does not perform any specific actions in this implementation.
+         */
         ~fifo_list() {};
-
+        
         void push(const T& _data) override {
             if (this->length != 0) {
                 LINK* temp = new LINK(_data);
